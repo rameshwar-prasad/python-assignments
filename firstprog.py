7180c048c23db7df08b982c56be1f7b0546e6b06
rameshwar-prasad <rameshwarprasad.biz@gmail.com>
2025-06-20 13:09:19 +0530

    Add files via upload

---
+++ firstprog.py
@@ -0,0 +1,7 @@
+m1 = float(input('m1: Enter first value: '))
+m2 = float(input('m2: Enter second value: '))
+
+print('Addition:', m1 + m2)
+print('Subtraction:', m1 - m2)
+print('Multiplication:', m1 * m2)
+print('Division:', m2 / m1 if m1 != 0 else 'Cannot divide by zero')
---
+++ secondprog.py
@@ -0,0 +1,3 @@
+fname = input('fname: Enter First Name: ')
+lname = input('lname: Enter Last Name: ')
+print('Hello', fname + ' ' + lname, 'Welcome to python programming')
