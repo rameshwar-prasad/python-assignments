c8499a0f62925acf05faf58e508ba36ba588d6e6
rameshwar-prasad <rameshwarprasad.biz@gmail.com>
2025-06-20 17:16:30 +0530

    Add files via upload

---
+++ thirdprog.py
@@ -0,0 +1,10 @@
+# Program to check if a number is even or odd
+
+# Take input from the user
+num = int(input("Enter a number: "))
+
+# Check if the number is even or odd
+if num % 2 == 0:
+    print(num, "is Even")
+else:
+    print(num, "is Odd")
