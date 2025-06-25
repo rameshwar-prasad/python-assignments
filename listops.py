daf5c186dbde2780fe04c51fa3a0da87b41c19f8
rameshwar-prasad <rameshwarprasad.biz@gmail.com>
2025-06-25 12:56:03 +0530

    Add files via upload

---
+++ listops.py
@@ -0,0 +1,5 @@
+numbers=list(range(1,11))
+first_five=(numbers[:5])
+print("First five elements:", first_five)
+reversed_first_five = first_five[::-1]
+print("Reversed first five elements:", reversed_first_five)
